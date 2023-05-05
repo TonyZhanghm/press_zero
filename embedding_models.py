fe9a5933b8543b86a711debdeb319b9dc3b7b5dc
sguo <sguo@trn81.phx15.castle.fm>
2023-05-05 00:44:34 +0000

    add SentenceTransformer embedding class

---
+++ embedding_models.py
@@ -0,0 +1,14 @@
+from sentence_transformers import SentenceTransformer
+
+SUPPORTED_MODELS = ['multi-qa-MiniLM-L6-cos-v1', 'multi-qa-distilbert-dot-v1', 'multi-qa-mpnet-base-dot-v1']
+
+class SentenceTransformerEmbeddingModel():
+    def __init__(
+        model_name
+    ):
+    if model_name not in SUPPORTED_MODELS:
+        raise Exception('unsupported embedding model')
+    self.model = SentenceTransformer('model_name')
+
+    def encode(self, text):
+        return self.model.encode(text)
